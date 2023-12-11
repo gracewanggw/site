7b99a990ccb978ba5ca8d4ae732fdb5f9d9f9648
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-11 16:33:37 -0600

    about and projects

--- README.md
+++
@@ -1,2 +0,0 @@
-# gracewang
-personal website
---
+++ website/src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from "react";
+import skills from "../data";
+
+function Skills() {
+    return (
+        <section id='skills'>
+
+        </section>
+    );
+}
+
+export default Skills;
