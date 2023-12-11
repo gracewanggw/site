04ea2b4f22ea578d5d0894ea209fb4c86bc35084
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-11 17:03:56 -0600

    experience

--- website/src/components/Projects.js
+++ website/src/components/Projects.js
@@ -7,32 +7,20 @@ import ProjectCard from "./ProjectCard";
 function Projects(){
     return (
         <section id='projects'>
-            <Container maxWidth="md">
+            <Container maxWidth="lg">
+                <h1 style={{textAlign: 'center'}}>
+                    Projects
+                </h1>
+                <h3 style={{textAlign: 'center'}}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+                    laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+                    Laborum, voluptas natus?
+                </h3>
                 <Grid container spacing={3}>
                     { projects.map((project) => (
                         <Grid item xs={6}>
                             <ProjectCard project={project}/>
                         </Grid>
-                        // <a>
-                        //     href={project.link}
-                        //     key={project.image}
-                        //     <div className="flex relative">
-                        //         <img
-                        //         alt="gallery"
-                        //         className="absolute inset-0 w-full h-full object-cover object-center"
-                        //         src={project.image}
-                        //         />
-                        //         <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                        //         <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                        //             {project.subtitle}
-                        //         </h2>
-                        //         <h1 className="title-font text-lg font-medium text-white mb-3">
-                        //             {project.title}
-                        //         </h1>
-                        //         <p className="leading-relaxed">{project.description}</p>
-                        //         </div>
-                        //     </div>
-                        // </a>
                         )
                     )}
                 </Grid>
--- website/src/data.js
+++ website/src/data.js
@@ -33,4 +33,29 @@ export const projects = [
     },
   ];
 
-export const skills = ["Java", "Python", "C", "JavaScript"]
+export const experience = [
+    {
+        title: "Data Scientist, Software Engineer, and Mentor",
+        company: "DALI Lab",
+        companyLink: "https://dali.dartmouth.edu/",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        startDate: "03/2021",
+        endDate: "present"
+    },
+    {
+        title: "Software Engineering Intern",
+        company: "Goldman Sachs",
+        companyLink: "https://www.goldmansachs.com/",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        startDate: "06/2023",
+        endDate: "08/2023"
+    },
+    {
+        title: "Software Engineering and Data Science Intern",
+        company: "Johns Hopkins Univeristy Applied Physics Lab",
+        companyLink: "https://www.jhuapl.edu/",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        startDate: "12/2022",
+        endDate: "03/2023"
+    }
+]
