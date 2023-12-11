f145a04d920c65dc45e0a8fd5751b93a50868a3a
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-11 17:03:56 -0600

    experience

--- website/src/App.js
+++ website/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <main>
 		<Navbar />
 		<About />
+		<Experience />
 		<Projects />
-		<Skills />
 	</main>
   );
 }
