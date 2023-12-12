b6550bb400f564aa3702d302e450e08a4288f68f
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-12 00:29:32 -0600

    change background color

---
+++ src/components/ContactForm.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// import Container from "@mui/material/Container";
+// import Grid from "@mui/material/Grid";
+// import TextField from "@mui/material/TextField";
+// import FormControl from '@mui/material/FormControl';
+
+// function ContactForm(){
+//     return (
+//         <Container maxWidth='xl'>
+//             <Grid container spacing={3}>
+//                 <Grid item>
+//                     <TextField required id="outlined-basic" label="Outlined" variant="outlined" />
+//                 </Grid>
+//             </Grid>
+//             <Grid container>
+//                 <Grid item sx={12}>
+//                     <FormControl fullWidth sx={{ m: 1 }}>
+//                         <TextField
+//                             id="outlined-multiline-flexible"
+//                             label="Multiline"
+//                             multiline
+//                             maxRows={6}
+//                         />
+//                     </FormControl>
+//                 </Grid>
+//             </Grid>
+//         </Container>
+//     )
+// }
+
+// export default ContactForm;
