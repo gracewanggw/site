0f41cd7060fc47765f850002b99b134b78758825
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-13 02:35:47 -0600

    project card mobile sizing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,7 +15,7 @@ function Projects(){
                 </h1>
                 <Grid container spacing={4} style={{padding: '12px'}} >
                     { projects.map((project) => (
-                        <Grid item xs={6} style={{boxShadow: 12}}>
+                        <Grid item xs={12} sm={6} md={6} lg={6} style={{boxShadow: 12}}>
                             <ProjectCard project={project}/>
                         </Grid>
                         )
