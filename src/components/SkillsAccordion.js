a14043d7f1528cb425563c828c7c6cd4ed4d2062
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-13 00:31:04 -0600

    make compatible with mobile

--- src/components/SkillsAccordion.js
+++ src/components/SkillsAccordion.js
@@ -23,7 +23,7 @@ function SkillsAccordion({title, skills}){
             <AccordionDetails alignContent='center'>
                 <Grid container item spacing={2} alignContent='center' >
                     {skills.map((skill, index) => (
-                        <Grid item xs={6} sm={4} md={3} lg={1} key={index} alignContent='center' >
+                        <Grid item xs={4} sm={3} md={2} lg={1} key={index} alignContent='center' >
                             <img 
                                 src={skill.logo}
                                 alt={skill.name}
