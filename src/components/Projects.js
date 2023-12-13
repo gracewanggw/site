33e14c0bd6b894ff2c017891cde0e151f89dcef6
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-13 02:29:13 -0600

    add drop shadows

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,7 +15,7 @@ function Projects(){
                 </h1>
                 <Grid container spacing={4} style={{padding: '12px'}} >
                     { projects.map((project) => (
-                        <Grid item xs={6}>
+                        <Grid item xs={6} style={{boxShadow: 12}}>
                             <ProjectCard project={project}/>
                         </Grid>
                         )
