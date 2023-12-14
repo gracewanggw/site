3a5f7826d5d2555c0f5391f379c3cea875b45825
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-14 01:46:49 -0600

    center skills

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -17,10 +17,10 @@ const ProjectDetail = ({ project }) => {
         color: 'white',
       }}
     >
-      <h1 style={{fontSize: '10%'}}>{project.title}</h1>
-      <h3 style={{fontSize: '6%'}}>{project.subtitle}</h3>
-      <p style={{ fontSize: '4%', textAlign: 'center', marginLeft: '1vw', marginRight:'1vw'}}>{project.description}</p>
-      <p style={{ fontSize: '4%', textAlign: 'center', marginLeft: '1vw', marginRight:'1vw'}}>Tech Stack: {project.stack}</p>
+      <h1 >{project.title}</h1>
+      <h3 >{project.subtitle}</h3>
+      <p style={{ textAlign: 'center', marginLeft: '1vw', marginRight:'1vw'}}>{project.description}</p>
+      <p style={{ textAlign: 'center', marginLeft: '1vw', marginRight:'1vw'}}>Tech Stack: {project.stack}</p>
     </div>
   );
 };
