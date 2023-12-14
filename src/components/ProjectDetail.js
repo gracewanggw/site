2644d67e9f0150d50aaeefa3ef3799fa91d53e8f
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-14 01:54:16 -0600

    font size

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
 const ProjectDetail = ({ project }) => {
+  const containerRef = useRef(null);
+  const [fontSize, setFontSize] = useState('2vw');
+
+  useEffect(() => {
+    const calculateFontSize = () => {
+      const containerWidth = containerRef.current.offsetWidth;
+      const numberOfColumns = 2; // Change this based on your layout
+      const baseFontSize = 2; // Change this based on your design
+      const calculatedFontSize = (containerWidth / numberOfColumns) / baseFontSize;
+      setFontSize(`${calculatedFontSize}vw`);
+    };
+
+    calculateFontSize();
+
+    // Recalculate on window resize
+    window.addEventListener('resize', calculateFontSize);
+
+    return () => {
+      window.removeEventListener('resize', calculateFontSize);
+    };
+  }, []);
+
   return (
     <div
       style={{
@@ -17,8 +39,8 @@ const ProjectDetail = ({ project }) => {
         color: 'white',
       }}
     >
-      <h1 >{project.title}</h1>
-      <h3 >{project.subtitle}</h3>
+      <h1 style={{fontSize: {fontSize}}} >{project.title}</h1>
+      <h3 style={{fontSize: 0.7*{fontSize}}}>{project.subtitle}</h3>
       <p style={{ textAlign: 'center', marginLeft: '1vw', marginRight:'1vw'}}>{project.description}</p>
       <p style={{ textAlign: 'center', marginLeft: '1vw', marginRight:'1vw'}}>Tech Stack: {project.stack}</p>
     </div>
