87bbbb3d1bd62360d531373166fe2b6c099b0a66
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-18 02:06:01 -0600

    added commit graph

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { experience } from "../data";
 import Divider from "@mui/material/Divider";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import LineChart from "./LineChart";
 
 function Experience() {
+    const lineChartRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.5, // Adjust this threshold as needed
+        };
+
+        const observer = new IntersectionObserver(handleIntersection, options);
+
+        if (lineChartRef.current) {
+            observer.observe(lineChartRef.current);
+        }
+
+        return () => {
+            if (lineChartRef.current) {
+            observer.unobserve(lineChartRef.current);
+            }
+        };
+    }, []);
+
+    const handleIntersection = (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+            setIsVisible(true);
+            }
+        });
+    };
+
     return (
         <section id='experience' style={{paddingTop: '48px', paddingBottom:'12px'}}>
             <Divider></Divider>
@@ -27,6 +59,9 @@ function Experience() {
                     )}
                 </Grid>
             </Container>
+            <Grid item xs={12} ref={lineChartRef} >
+                {isVisible && <LineChart />}
+            </Grid> 
         </section>
     );
 }
