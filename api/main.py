03a1f364bed516410d2b0dfe2278036404583c56
gracewanggw <grace.wang.24@dartmouth.edu>
2023-12-18 13:16:50 -0600

    cors fix

--- api/main.py
+++ api/main.py
@@ -76,11 +76,18 @@ def get_data():
 
 
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
